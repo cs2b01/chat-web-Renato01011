269dfd99a7e17ef14b18be5ff8495f7bb81ac3e5
Renato01011 <40170602+Renato01011@users.noreply.github.com>
2019-05-29 12:15:40 -0500

    Add files via upload

--- web/server.py
+++ web/server.py
@@ -17,7 +17,7 @@ def static_content(content):
     return render_template(content)
 
 
-@app.route('/users', methods = ['GET'])
+@app.route('/users', methods=['GET'])
 def get_users():
     session = db.getSession(engine)
     dbResponse = session.query(entities.User)
@@ -26,8 +26,7 @@ def get_users():
         data.append(user)
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
 
-
-@app.route('/users/<id>', methods = ['GET'])
+@app.route('/users/<id>', methods=['GET'])
 def get_user(id):
     db_session = db.getSession(engine)
     users = db_session.query(entities.User).filter(entities.User.id == id)
@@ -45,6 +44,42 @@ def create_test_users():
     db_session.add(user)
     db_session.commit()
     return "Test user created!"
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    c =  json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    return 'Created User'
+
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c =  json.loads(request.form['values'])
+    for key in c.keys():
+        setattr(user, key, c[key])
+    session.add(user)
+    session.commit()
+    return 'Updated User'
+
+@app.route('/users', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    messages = session.query(entities.User).filter(entities.User.id == id)
+    for message in messages:
+        session.delete(message)
+    session.commit()
+    return "Deleted Message"
 
 if __name__ == '__main__':
     app.secret_key = ".."
